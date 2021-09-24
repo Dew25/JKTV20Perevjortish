208e849a7bfcdf0be10200708a474b14a2dac7b2
TeacherIVKHK <teacherIvkhk@ivkhk.ee>
2021-09-24 10:57:05 +0300

    Работаем со строкой

---
+++ src/jktv20perevjortish/JKTV20Perevjortish.java
@@ -0,0 +1,39 @@
+/**
+ * Программа просит ввести строку и выводит ее наоборот
+ * 
+ */
+package jktv20perevjortish;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Melnikov
+ */
+public class JKTV20Perevjortish {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Введите строку: ");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+//        System.out.println("str = "+str);
+        char[] charStr = str.toCharArray();
+//        System.out.println("charStr = "+Arrays.toString(charStr));
+        char[] newCharStr = new char[charStr.length];
+//        System.out.println("newCharStr = "+Arrays.toString(newCharStr));
+        for (int i = 0; i < charStr.length; i++) {
+            newCharStr[(charStr.length-1)-i]=charStr[i];
+        }
+//        System.out.println("newCharStr = "+Arrays.toString(newCharStr));
+        String str2 = new String(newCharStr);
+        System.out.println("Строка наоборот: "+str2);
+        System.out.println("+++++++++++++++++++++++++++++++++++");
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println("Строка наоборот: " + sb.reverse().toString());
+    }
+    
+}
